b925b6c89d3eb55d7b32bac535f852df1506b9ba
hannguyen1 <han.nguyen@phase2industries.com>
2017-07-12 17:26:57 -0700

    Custom boilerplate + Adopt a pet appkey

--- SampleApp/app/src/main/java/com/p2ux/sampleapp/MainActivity.java
+++ SampleApp/app/src/main/java/com/p2ux/sampleapp/MainActivity.java
@@ -2,17 +2,23 @@ package com.p2ux.sampleapp;
 
 import android.os.Bundle;
 
-import com.p2i.uxcore.utils.UXLog;
 import com.p2i.uxmobileapp.UXMobileAppFragmentActivity;
+import com.p2i.uxmobileapp.components.UXMobileAppBehavior;
 
 public class MainActivity extends UXMobileAppFragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        mAppKey = ""; // APP KEY
-        mLogLevel = UXLog.UXCoreLogLevelVerbose;
-        setStaged(true);
+    public void onCreate(Bundle savedInstanceState) {
+        //You can get the app key or license key from builder for your app. This is required to link your builder up to your local app
+        mAppKey = "0006.f058b95ac4e851b47424d19ff2cd4bf69ec70383187cb5fd0b02a2978f08d13e";
 
         super.onCreate(savedInstanceState);
+        applyTranslucentStatusBar();
     }
+
+    //This is a method from UXMobileAppBehavior
+    // Here you call on your custom app behavior class
+    @Override
+    public UXMobileAppBehavior createBehavior(String appId) { return new SampleAppTestBehavior(getApplicationContext());}
+
 }
